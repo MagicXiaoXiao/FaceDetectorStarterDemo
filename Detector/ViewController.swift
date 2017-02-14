692ecc950ac0e736d49c2a1791b04ec09911a324
晓晓魔导师 <1171856045@qq.com>
2017-02-14 15:09:51 +0800

    图片人脸识别Demo

---
+++ Detector/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Detector
+//
+//  Created by Gregg Mojica on 8/21/16.
+//  Copyright © 2016 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var personPic: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        personPic.image = UIImage(named: "face-1")
+        
+        detect()
+        
+    }
+    
+    func detect() -> Void{
+        guard let personciImage = CIImage(image: personPic.image!) else {
+            return
+        }
+        
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector?.features(in: personciImage)
+        
+        let ciImageSize = personciImage.extent.size
+        var transfrom = CGAffineTransform(scaleX: 1, y: -1)
+        transfrom = transfrom.translatedBy(x: 0, y: -ciImageSize.height)
+        
+        for face in faces as! [CIFaceFeature] {
+            
+            print("Found bounds are \(face.bounds)")
+            
+            var faceViewBounds = face.bounds.applying(transfrom)
+            
+            let viewSize = personPic.bounds.size
+            let scale = min(viewSize.width / ciImageSize.width,
+                            viewSize.height / ciImageSize.height)
+            
+            let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
+            let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
+            
+            faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+            
+            faceViewBounds.origin.x += offsetX
+            faceViewBounds.origin.y += offsetY
+            
+            let faceBox = UIView(frame: faceViewBounds)
+            faceBox.layer.borderWidth = 3
+            faceBox.layer.borderColor = UIColor.red.cgColor
+            faceBox.backgroundColor = UIColor.clear
+            personPic.addSubview(faceBox)
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.rightEyePosition)")
+            }
+            
+        }
+        
+    }
+    
+}
